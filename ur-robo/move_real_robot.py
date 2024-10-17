bb41f83fc8f6215185a2fe13579fdb4953a622a2
Yelin Zhang <30687616+Yelinz@users.noreply.github.com>
2024-10-17 15:32:40 +0200

    Update move_real_robot.py

--- ur-robo/move_real_robot.py
+++ ur-robo/move_real_robot.py
@@ -43,37 +43,52 @@ class RealRobotArm:
 if __name__ == '__main__':
     robot = RealRobotArm()
 
-    sleep_const = 3
-
     # send joint angles
     joint_angles = [0, -1.57, 0, 0, 0, 0] # upright position
     robot.send_joint_command(joint_angles)
-    rospy.sleep(sleep_const)
+    robot.send_gripper_command(50)
+    rospy.sleep(5)
 
-    start_angles = calculate_inverse_kinematics([-0.25, 0.45, 0.15, 0, 3.14, 3.14/2], joint_angles)
-    robot.send_joint_command(start_angles)
-    rospy.sleep(sleep_const)
-
-    # set requested postion to 100 (value between 0-255)
-    robot.send_gripper_command(0)
-    rospy.sleep(sleep_const)
-    
-    lift_angles = calculate_inverse_kinematics([-0.25, 0.45, 0.31, 0, 3.14, 3.14/2], start_angles)
+    lift_angles = calculate_inverse_kinematics([1.30, -0.45, 0.31, 0, 0, 3.14/2], joint_angles)
     robot.send_joint_command(lift_angles)
-    rospy.sleep(sleep_const)
+    rospy.sleep(4)
 
-    move_angles = calculate_inverse_kinematics([0.25, 0.45, 0.31, 0, 3.14, 3.14/2], lift_angles)
+    start_angles = calculate_inverse_kinematics([0.30, -0.45, 0.17, 0, 0, 3.14/2], lift_angles)
+    print("step 1", start_angles)
+    robot.send_joint_command(start_angles)
+    rospy.sleep(1)
+    robot.send_gripper_command(220)
+    rospy.sleep(3)
+
+    lift_angles = calculate_inverse_kinematics([0.30, -0.45, 0.31, 0, 0, 3.14/2], start_angles)
+    print("step 2", lift_angles)
+    robot.send_joint_command(lift_angles)
+    rospy.sleep(2)
+
+    center_angles = calculate_inverse_kinematics([0, -0.45, 0.35, 0, 0, 3.14/2], lift_angles)
+    print("step 3", center_angles)
+    robot.send_joint_command(center_angles)
+    rospy.sleep(2)
+
+    move_angles = calculate_inverse_kinematics([-0.30, -0.45, 0.31, 0, 0, 3.14/2], center_angles)
+    print("step 4", move_angles)
     robot.send_joint_command(move_angles)
-    rospy.sleep(sleep_const)
+    rospy.sleep(2)
 
-    drop_angles = calculate_inverse_kinematics([0.25, 0.45, 0.15, 0, 3.14, 3.14/2], move_angles)
+    drop_angles = calculate_inverse_kinematics([-0.30, -0.45, 0.18, 0, 0, 3.14/2], move_angles)
+    print("step 5", drop_angles)
     robot.send_joint_command(drop_angles)
-    rospy.sleep(sleep_const)
+    rospy.sleep(2)
 
-    robot.send_gripper_command(100)
-    rospy.sleep(sleep_const)
+    robot.send_gripper_command(0)
+    rospy.sleep(2)
+
+    move_angles = calculate_inverse_kinematics([-0.30, -0.45, 0.31, 0, 0, 3.14/2], drop_angles)
+    print("step 6", move_angles)
+    robot.send_joint_command(move_angles)
+    rospy.sleep(2)
 
     robot.send_joint_command(joint_angles)
-    rospy.sleep(sleep_const)
+    rospy.sleep(1)
 
     robot.close_connection()
