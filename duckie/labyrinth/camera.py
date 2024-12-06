ab89815a137dd6c0896b01efc421d2ab8095ea3f
Yelin Zhang <30687616+Yelinz@users.noreply.github.com>
2024-12-06 11:30:45 +0100

    Update camera.py

--- duckie/labyrinth/camera.py
+++ duckie/labyrinth/camera.py
@@ -1,127 +1,231 @@
 #!/usr/bin/python3
 
 import rospy
-import cv2
-from cv_bridge import CvBridge
-from sensor_msgs.msg import CompressedImage
+import math
 import numpy as np
+import tf2_ros as tf
+from simple_pid import PID
+from std_msgs.msg import Float64
 
-class CameraSubscriber:
+import geometry_msgs.msg
+from duckietown_msgs.msg import WheelsCmdStamped, WheelEncoderStamped#, Twist2DStamped
+from std_msgs.msg import Header
+from camera import CameraSubscriber
+from tf.transformations import quaternion_from_euler
+
+class DifferentialSteering:
     def __init__(self, robot_name):
-        # subscribe to a topic of type CompressedImage  
-        topic = '/' + robot_name + '/camera_node/image/compressed'
-        # when a message is received, the callback is invoked
-        rospy.Subscriber(topic, CompressedImage, self.callback)
-        rospy.sleep(2.0) # needed to make sure the node is indeed initialized
+        self.robot_name = robot_name
+        rospy.init_node('differential_steering', anonymous=True)
         
-        # create a openCV <-> ROS bridge
-        self.cv2_bridge = CvBridge()
-        self.rate = rospy.Rate(20)  # the node is running at 10 hz
-
-    def callback(self, data):
-        # the callback should be light and fast
-        self.image = data
-
-    def angle_diff(self, debug=False):
-        # image processing is done on the latest image received
-        img = self.cv2_bridge.compressed_imgmsg_to_cv2(self.image, "bgr8")
-        # cutoff the top half of the image, that part does not matter
-        img = img[img.shape[0]//2:, :]
-
-        filtered, mask = self.filter_line(img)
-        if debug:
-            cv2.imwrite('./raw.png', img)
-            cv2.imwrite('./filtered.png', filtered)
-
-        frame_height, frame_width, _ = filtered.shape
-
-        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        # Find the largest contour, assuming it is the line
-        if contours is not None:
-            contour = max(contours, key=cv2.contourArea)
-            line_x = self.get_line_position(contour, frame_width)
-
-            # Draw the contour and center line for visualization
-            cv2.drawContours(filtered, [contour], -1, (0, 255, 0), 3)
-            cv2.line(filtered, (line_x, 0), (line_x, frame_height), (255, 0, 0), 2)
-            if debug:
-                cv2.imwrite('./contour.png', filtered)
-
-            # Correct the robot's direction
-            center_x = frame_width // 2
-            center_diff = line_x - center_x
-            return np.arctan(center_diff / frame_height)
-
-    def visual_rotation(self, debug=False):
-        # image processing is done on the latest image received
-        img = self.cv2_bridge.compressed_imgmsg_to_cv2(self.image, "bgr8")
-        # cutoff the top half of the image, that part does not matter
-        img = img[img.shape[0]//2:, :]
-
-        filtered, mask = self.filter_line(img)
-        if debug:
-            cv2.imwrite('./raw.png', img)
-            cv2.imwrite('./filtered.png', filtered)
-
-        frame_height, frame_width, _ = filtered.shape
-
-        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        # Find the largest contour, assuming it is the line
-        if contours is not None:
-            contour = max(contours, key=cv2.contourArea)
-            line_x = self.get_line_position(contour, frame_width)
-
-            # Draw the contour and center line for visualization
-            cv2.drawContours(filtered, [contour], -1, (0, 255, 0), 3)
-            cv2.line(filtered, (line_x, 0), (line_x, frame_height), (255, 0, 0), 2)
-            if debug:
-                cv2.imwrite('./contour.png', filtered)
-
-            # Correct the robot's direction
-            center_x = frame_width // 2
-            tolerance = 15
-            if line_x < center_x - tolerance:
-                print('turn left')
-                return 1
-            elif line_x > center_x + tolerance:
-                print('turn right')
-                return -1
-            else:
-                print("Moving forward...")
-                return 0
-        else:
-            print("no line detected")
-
+        # create publisher for wheel commands
+        wheel_command_topic = '/' + robot_name + '/wheels_driver_node/wheels_cmd'
+        self.wheel_command_publisher = rospy.Publisher(wheel_command_topic, WheelsCmdStamped, queue_size = 10)
         
-    def filter_line(self, img):
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # subscribe wheel tick messages
+        left_wheel_tick_topic = '/' + robot_name + '/left_wheel_encoder_node/tick'
+        right_wheel_tick_topic = '/' + robot_name + '/right_wheel_encoder_node/tick'
+        left_msg = rospy.wait_for_message(left_wheel_tick_topic, WheelEncoderStamped)
+        right_msg = rospy.wait_for_message(right_wheel_tick_topic, WheelEncoderStamped)
+        # Tick to position variables
+        self.left_resolution = left_msg.resolution
+        self.right_resolution = right_msg.resolution
+        self.initial_left_tick = left_msg.data
+        self.initial_right_tick = right_msg.data
+        self.left_tick_count = self.initial_left_tick
+        self.right_tick_count = self.initial_right_tick
+        self.prev_left_tick = 0
+        self.prev_right_tick = 0
+        # Constants
+        self.axis_width = 0.1
+        self.radius = 0.0318
+        self.max_speed = 0.2
+        self.tile_width = 0.3
+        # Calculated position
+        self.x = 0
+        self.y = 0
+        self.angle = 0
 
-        lower_yellow = np.array([15, 20, 50])
-        upper_yellow = np.array([60, 255, 255])
+        rospy.Subscriber(left_wheel_tick_topic, WheelEncoderStamped, self.callback_left_wheel_tick)
+        rospy.Subscriber(right_wheel_tick_topic, WheelEncoderStamped, self.callback_right_wheel_tick)
+        # self.control_pub = rospy.Publisher("/duckiebot_control", Twist2DStamped, queue_size=10)
 
-        #create a mask for green colour using inRange function
-        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-        return cv2.bitwise_and(img, img, mask=mask), mask
+        # rospy.Subscriber("/current_speed", Float64, self.speed_callback)
+        # rospy.Subscriber("/current_steering_angle", Float64, self.steering_callback)
+
+
+        self.transform_broadcaster = tf.TransformBroadcaster()
+
+        self.camera = CameraSubscriber(robot_name)
+
+        self.speed_pid = PID(kp=1.0, ki=0.01, kd=0.1)  # For speed control
+        self.steering_pid = PID(kp=1.5, ki=0.05, kd=0.2)  # For steering control
+
+        # set the robot at pose (0,0,0)
+        self.publish_transform(x=0, y=0, theta=0)
+       
+        # Current state variables
+        self.current_speed = 0.0
+        self.current_steering_angle = 0.0
+
+    # def speed_callback(self, msg):
+    #     """Callback to update current speed."""
+    #     self.current_speed = msg.data
+
+    # def steering_callback(self, msg):
+    #     """Callback to update current steering angle."""
+    #     self.current_steering_angle = msg.data
+
+
+    def callback_left_wheel_tick(self, data):
+        self.left_tick_count = data.data
+        self.calc_pose()
+        self.publish_transform(self.x, self.y, self.angle)
     
-    def get_line_position(self, contour, frame_width):
-        """Get the x-coordinate of the center of the line."""
-        M = cv2.moments(contour)
-        if M["m00"] > 0:
-            line_x = int(M["m10"] / M["m00"])
-            return line_x
-        return frame_width // 2  # Default to center if no contour
+    def callback_right_wheel_tick(self, data):
+        self.right_tick_count = data.data
+        self.calc_pose()
+        self.publish_transform(self.x, self.y, self.angle)
+
+    def turn_wheels(self, velocity_left=0, velocity_right=0):
+        command = WheelsCmdStamped()
+        command.header = Header()
+        command.vel_left = velocity_left
+        command.vel_right = velocity_right
+        command.header.stamp = rospy.Time.now()
+        self.wheel_command_publisher.publish(command)
+
+
+    def rotate_to_angle(self, target_angle, tolerance=0.07):
+        """
+        Rotate the robot to face a specific target angle.
+        """
+        while not rospy.is_shutdown():
+            angle_difference = self.angle - target_angle
+
+            # Normalize the angle to be within -pi to +pi
+            angle_difference = math.atan2(math.sin(angle_difference), math.cos(angle_difference))
+
+            if abs(angle_difference) > 0.7:
+                print("Fixed rotation")
+                # Rotate in place: if angle difference is positive, turn right; else, turn left
+                if angle_difference > 0:
+                    self.turn_wheels(self.max_speed, -self.max_speed)
+                else:
+                    self.turn_wheels(-self.max_speed, self.max_speed)
+            else:
+                print("Visual rotation")
+                direction = self.camera.visual_rotation()
+                self.turn_wheels(-self.max_speed * direction, self.max_speed * direction)
+                if direction == 0:
+                    break
+            rospy.sleep(0.1)
+
+    def move_to_point(self, target_x, target_y, target_angle=None, tolerance=0.05):
+        """
+        Move the robot from its current position to a target point (target_x, target_y).
+        """
+        if target_angle is None:
+            target_angle = math.atan2(target_y - self.y, target_x - self.x)
+        
+        pid = PID(1, 0.01, 0.01, setpoint=1)
+        
+        while not rospy.is_shutdown():
+            # Calculate distance and angle to the target
+            distance_to_target = math.sqrt((target_x - self.x)**2 + (target_y - self.y)**2)
+
+            # Check if we have reached the target within tolerance
+            if distance_to_target < tolerance:
+                self.turn_wheels(0, 0)
+                print("Reached the target!")
+                break
+            
+            # Move forward once aligned
+            move_speed = min(self.max_speed*2, distance_to_target)
+            self.turn_wheels(move_speed, move_speed)
+
+            # Regler: Rotate to face the target angle
+            angle_error = self.camera.angle_diff()
+            print('angle error:', angle_error)
+            output = pid(angle_error)
+            print('regler: ', output)
+
+            steering_control = self.steering_pid.pid(angle_error)
+            if angle_error < 0:
+                move_speed_left = move_speed + steering_control
+                move_speed_right = move_speed - steering_control
+                self.turn_wheels(move_speed_left, move_speed_right)
+            else:
+                move_speed_right = move_speed + steering_control
+                move_speed_left = move_speed - steering_control
+                self.turn_wheels(move_speed_left, move_speed_right)
+
+            # Publish the control commands
+            control_msg = Twist2DStamped()
+            control_msg.v = speed_control  # Speed control
+            control_msg.omega = steering_control  # Steering control
+            self.control_pub.publish(control_msg)
+
+            rospy.loginfo(f'distance: {distance_to_target}, target angle: {target_angle}, speed: {move_speed}')
+            # Small delay to allow for control updates
+            rospy.sleep(0.1)
 
     def run(self):
-        while not rospy.is_shutdown():
-            self.visual_rotation()
-            self.rate.sleep()
+        self.turn_wheels()
+        #self.rotate_to_angle(1.5)
+        self.move_to_point(0.5, 0)
+        rospy.loginfo(f"end x: {self.x}, y: {self.y}, theta: {self.angle}")
+        self.turn_wheels()
+        
+    def publish_transform(self, x, y, theta):
+        """publishes a transform between the map frame and the robot_name/base frame
+                Parameters:
+                    x = relative x position
+                    y = relative y position
+                    theta = relative rotation around z-axis
+                    robot_name = name of duckiebot
+        """
+        q = quaternion_from_euler(0, 0 , theta)
 
+        t = geometry_msgs.msg.TransformStamped()
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = "map"
+        t.child_frame_id = f"{robot_name}/base" 
+        t.transform.translation.x = x
+        t.transform.translation.y = y
+        t.transform.translation.z = 0.0
+        t.transform.rotation.x = q[0]
+        t.transform.rotation.y = q[1]
+        t.transform.rotation.z = q[2]
+        t.transform.rotation.w = q[3]
+        # self.transform_broadcaster.sendTransform((x, y, 0.0), q, rospy.Time.now(),  robot_name + "/base", "map")
+
+        self.transform_broadcaster.sendTransform(t)
+
+    def calc_pose(self) -> None:
+        left_tick = self.left_tick_count - self.initial_left_tick - self.prev_left_tick
+        right_tick = self.right_tick_count - self.initial_right_tick - self.prev_right_tick
+
+        # calculating wheel displacements
+        distance_travelled_left_wheel = (2*math.pi*self.radius*left_tick) / self.left_resolution
+        distance_travelled_right_wheel = (2*math.pi*self.radius*right_tick) / self.right_resolution
+
+        # calculating robot's linear and angular displacements
+        displacement = (distance_travelled_left_wheel + distance_travelled_right_wheel) / 2
+        delta_theta = (distance_travelled_right_wheel - distance_travelled_left_wheel) / self.axis_width
+
+        # updating the pose
+        self.x = self.x + displacement * math.cos(self.angle + (delta_theta/2))
+        self.y = self.y + displacement * math.sin(self.angle + (delta_theta/2))
+        # self.angle = (self.angle + delta_theta) % 2*math.pi
+        # self.angle = self.angle + delta_theta
+        a = self.angle + delta_theta
+        self.angle = np.arctan2(np.sin(a), np.cos(a))
+        self.prev_left_tick += left_tick
+        self.prev_right_tick += right_tick
+        rospy.loginfo_throttle(0.2, f'x {self.x} y {self.y} angle {self.angle}')
 
 if __name__ == '__main__':
     robot_name = "phi"
-    # initialize a node with a name, annonymous=True ensures that the name is unique
-    rospy.init_node('camera_listener', anonymous=True)
-    cs = CameraSubscriber(robot_name)
-    cs.run()
+    ds = DifferentialSteering(robot_name)
+    ds.run()
